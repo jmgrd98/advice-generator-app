17d82116af4e671bfcd6a67c1968268ef46b20fc
João Marcelo Dantas <jmgrd98@gmail.com>
2022-08-07 10:30:33 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let root = document.getElementById('root')
+const url = 'https://api.adviceslip.com/advice'
+let advice = document.getElementById('advice')
+
+function generateAdvice(){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        root.innerHTML = JSON.stringify(data.slip.advice)
+        advice.innerHTML = `Advice #${data.slip.id}`
+    })
+    .catch(error => root.innerHTML = ('Error!'))
+}
+
+generateAdvice()
