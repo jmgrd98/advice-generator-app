4e48135d5815a799516590f9b2d3f1a9e1952168
João Marcelo Dantas <jmgrd98@gmail.com>
2022-08-19 10:34:42 -0300

    button commit

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function generateAdvice(){
 }
 
 generateAdvice()
+
+function changeAdvice(){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        root.innerHTML = JSON.stringify(data.slip.advice)
+        advice.innerHTML = `Advice #${data.slip.id}`
+    })
+    .catch(error => root.innerHTML = ('Error!'))
+}
