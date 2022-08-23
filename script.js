3f805710c2600fd576aa178b8f9325a34e94f842
João Marcelo Dantas <jmgrd98@gmail.com>
2022-08-23 11:12:40 -0300

    JS coment commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let root = document.getElementById('root')
-const url = 'https://api.adviceslip.com/advice'
-let advice = document.getElementById('advice')
+let root = document.getElementById('root') // Pegando o elemento que irá renderizar o advice da API
+const url = 'https://api.adviceslip.com/advice' // Armazenando o endpoint da API em uma variável
+let advice = document.getElementById('advice') // Pegando o elemento que irá renderizar o número do advice da API
+
+// Função que gera a API através de um fetch
 
 function generateAdvice(){
     fetch(url)
@@ -12,7 +14,12 @@ function generateAdvice(){
     .catch(error => root.innerHTML = ('Error!'))
 }
 
+// Chamando a função de gerar frase
+
 generateAdvice()
+
+// Função que muda a frase toda vez que é clicado o botão
+// (a função é chamada na <button> do index.html através do evento onclick)
 
 function changeAdvice(){
     fetch(url)
