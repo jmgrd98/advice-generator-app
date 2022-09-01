8292cf37c99777113129cf079cdbebfe0965e69e
João Marcelo Dantas <jmgrd98@gmail.com>
2022-09-01 20:11:04 -0300

    theme buttons commit

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let root = document.getElementById('root') // Pegando o elemento que irá renderizar o advice da API
 const url = 'https://api.adviceslip.com/advice' // Armazenando o endpoint da API em uma variável
 let advice = document.getElementById('advice') // Pegando o elemento que irá renderizar o número do advice da API
-
+let css = document.getElementById('css')
+let LightBtn = document.getElementById('light')
+let DarkBtn = document.getElementById('dark')
 // Função que gera a API através de um fetch
 
 function generateAdvice(){
@@ -30,3 +32,15 @@ function changeAdvice(){
     })
     .catch(error => root.innerHTML = ('Error!'))
 }
+
+function changeToDarkMode(){
+    css.setAttribute('href', 'darkmode.css')
+}
+
+LightBtn.addEventListener('click', changeToDarkMode)
+
+function changeToLightMode(){
+    css.setAttribute('href', 'style.css')
+}
+
+DarkBtn.addEventListener('click', changeToLightMode)
